a5b037804904f4139fe91ecb03b473fd0ea152e3
Adem Gaygusuz <adem@ardweb.co.uk>
2015-05-25 16:52:45 +0100

    Ordered flights by date

--- flyguy/flights/models.py
+++ flyguy/flights/models.py
@@ -4,3 +4,6 @@ class Flight(models.Model):
     name = models.CharField(max_length=255)
     date = models.DateField()
     notes = models.TextField()
+
+    class Meta:
+        ordering = ('-date',)
