a3b6e2d17707f736e2a1d059fa0ea1abb9fad72c
Adem Gaygusuz <adem@ardweb.co.uk>
2015-05-26 00:23:54 +0100

    Added single flight view

--- flyguy/flights/serializers.py
+++ flyguy/flights/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class FlightSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Flight
-        fields = ('url', 'name', 'date', 'notes')
+        fields = ('id', 'url', 'name', 'date', 'notes')
