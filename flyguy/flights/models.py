943bf4a07b7af3d8ceb502242708cc74deaeb09d
Adem Gaygusuz <adem@ardweb.co.uk>
2015-08-21 16:19:55 +0100

    Made first/last name required

--- flyguy/flights/models.py
+++ flyguy/flights/models.py
@@ -52,8 +52,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     Email and password are required. Other fields are optional.
     """
     email = models.EmailField(_('email address'), max_length=254, unique=True)
-    first_name = models.CharField(_('first name'), max_length=30, blank=True)
-    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    first_name = models.CharField(_('first name'), max_length=30)
+    last_name = models.CharField(_('last name'), max_length=30)
     is_staff = models.BooleanField(
         _('staff status'),
         default=False,
