5bd3f3c642f6a13712caa0b85f9913faee7529c4
Adem Gaygusuz <adem@ardweb.co.uk>
2015-08-27 00:22:09 +0100

    Added allowed hosts and debug env

--- flyguy/settings.py
+++ flyguy/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv('DEBUG', False))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.appyharry.com'
+]
 
 
 # Application definition
